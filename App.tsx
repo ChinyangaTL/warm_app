47e354ff1a5e02c2a5c0d3fe9dc916eec2f70e52
Nicolas Cavallin <nicavallin@gmail.com>
2022-11-30 10:19:52 +0100

    add codepush update

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import {SafeAreaView, StyleSheet, Text} from 'react-native';
 const App = () => {
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Hello World, this is a new feature.</Text>
+      <Text>Hello World, this is a new feature. Updated by CodePush.</Text>
     </SafeAreaView>
   );
 };
