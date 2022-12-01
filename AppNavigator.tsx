89b481827edb88cebd1378d0fd07f4429666817e
Alberto Linares <albertolinares@h-alberto-linares.home>
2022-12-01 16:03:17 +0100

    add react navigation

---
+++ AppNavigator.tsx
@@ -0,0 +1,20 @@
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Tab1 from './screens/tab1';
+import Tab2 from './screens/tab2';
+import Tab3 from './screens/tab3';
+import Tab4 from './screens/tab4';
+
+const Tab = createBottomTabNavigator();
+
+const AppNavigator = () => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Tab1" component={Tab1} />
+      <Tab.Screen name="Tab2" component={Tab2} />
+      <Tab.Screen name="Tab3" component={Tab3} />
+      <Tab.Screen name="Tab4" component={Tab4} />
+    </Tab.Navigator>
+  );
+};
+
+export default AppNavigator;
---
+++ screens/tab2.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const Tab2 = () => {
+  return (
+    <View>
+      <Text>Content</Text>
+    </View>
+  );
+};
+
+export default Tab2;
