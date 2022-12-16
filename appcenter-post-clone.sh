a8ab2853b1c4ef4cf0e6e2cb67bcb60f0159a4b6
Nicolas Cavallin <nicavallin@gmail.com>
2022-12-16 11:25:31 +0100

    update post clone

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -1,1 +1,12 @@
-echo "hola"
+echo "123PROBANDO"
+echo "456PROBANDO" > /dev/null
+printf "\nCreating AppCenter key file\n\n"
+SLACK_MESSAGE=`curl --write-out '%{http_code}' \
+    --silent \
+    --output /dev/null \
+    --request POST \
+    --header 'Content-Type: application/json' \
+    --url '${{ secrets.SLACK_URL }}' \
+    --data '{
+      "text": "Hello from post clone",
+    }'`
