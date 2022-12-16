0a49ce9bdcb8522383978bc7bf8d7b7d6e48eaa1
Nicolas Cavallin <nicavallin@gmail.com>
2022-12-16 15:13:08 +0100

    fix 5

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -2,10 +2,12 @@ if [[ -z ${APPCENTER_ANDROID_VARIANT} && -z ${APPCENTER_ANDROID_MODULE} ]];
 then
   echo "is iOS"
   curl -X POST -H 'Content-type: application/json' --data '{"text":"Hello, from iOS!"}' $SLACK_URL
+  exit
 fi
 
 if [[ -z ${APPCENTER_XCODE_PROJECT} && -z ${APPCENTER_XCODE_SCHEME} ]];
 then
   echo "is Android"
   curl -X POST -H 'Content-type: application/json' --data '{"text":"Hello, from Android!"}' $SLACK_URL
+  exit
 fi
