c38d3238ea1daa4a4509aa8a6aaeb18a62413d9e
Nicolas Cavallin <nicavallin@gmail.com>
2022-12-19 12:59:39 +0100

    post1

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -2,6 +2,9 @@ if [[ -z ${APPCENTER_ANDROID_VARIANT} && -z ${APPCENTER_ANDROID_MODULE} ]];
 then
   echo "is iOS"
   curl -X POST -H 'Content-type: application/json' --data '{"text":"Hello, from iOS!"}' $SLACK_URL
+  curl -X POST -H 'Content-type: application/json' --data '{"text":"$APPCENTER_SOURCE_DIRECTORY/ios/holdedmobile/AppCenter-Config.plist"}' $SLACK_URL
+
+  echo "$APPCENTER_SOURCE_DIRECTORY/ios/holdedmobile/AppCenter-Config.plist"
   exit
 fi
 
