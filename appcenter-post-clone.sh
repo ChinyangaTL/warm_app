bd8484cbd02c8e91e6c0ae3bcfaff09f70dcb898
Nicolas Cavallin <nicavallin@gmail.com>
2022-12-19 18:12:51 +0100

    post20

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -15,8 +15,20 @@
 #   exit
 # fi
 
+# COMMIT_ID=$(git rev-parse HEAD)
+
+# echo $COMMIT_ID
+
+# curl -X POST -H 'Content-type: application/json' --data '{"text":"Hello, from $COMMIT_ID!"}' $SLACK_URL
+
 COMMIT_ID=$(git rev-parse HEAD)
 
 echo $COMMIT_ID
 
-curl -X POST -H 'Content-type: application/json' --data '{"text":"Hello, from $COMMIT_ID!"}' $SLACK_URL
+curl -X POST -H "Content-type: application/json" -H "Authorization: Bearer $GITHUB_TOKEN" \ 
+  --data '{
+    "commit_id": "'"$COMMIT_ID"'",
+    "build_id": "'"$APPCENTER_BUILD_ID"'",
+    "os": "'"$APPCENTER_BUILD_AGENT_OS"'",
+    "status": "'"$AGENT_JOBSTATUS"'",
+  }' https://api.github.com/repos/nicolascavallin/warm_app/actions/workflows/from_appcenter2.yml/dispatches
