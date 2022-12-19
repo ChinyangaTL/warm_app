825c645488cc52192f245f00ffd3a9d348939c31
Nicolas Cavallin <nicavallin@gmail.com>
2022-12-19 17:40:14 +0100

    post19

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -1,16 +1,22 @@
-if [[ -z ${APPCENTER_ANDROID_VARIANT} && -z ${APPCENTER_ANDROID_MODULE} ]];
-then
-  echo "is iOS"
-  curl -X POST -H 'Content-type: application/json' --data '{"text":"Hello, from iOS!"}' $SLACK_URL
-  curl -X POST -H 'Content-type: application/json' --data '{"text":"$APPCENTER_SOURCE_DIRECTORY/ios/holdedmobile/AppCenter-Config.plist"}' $SLACK_URL
+# if [[ -z ${APPCENTER_ANDROID_VARIANT} && -z ${APPCENTER_ANDROID_MODULE} ]];
+# then
+#   echo "is iOS"
+#   curl -X POST -H 'Content-type: application/json' --data '{"text":"Hello, from iOS!"}' $SLACK_URL
+#   curl -X POST -H 'Content-type: application/json' --data '{"text":"$APPCENTER_SOURCE_DIRECTORY/ios/holdedmobile/AppCenter-Config.plist"}' $SLACK_URL
 
-  echo "$APPCENTER_SOURCE_DIRECTORY/ios/holdedmobile/AppCenter-Config.plist"
-  exit
-fi
+#   echo $APPCENTER_SOURCE_DIRECTORY/ios/holdedmobile/AppCenter-Config.plist
+#   exit
+# fi
 
-if [[ -z ${APPCENTER_XCODE_PROJECT} && -z ${APPCENTER_XCODE_SCHEME} ]];
-then
-  echo "is Android"
-  curl -X POST -H 'Content-type: application/json' --data '{"text":"Hello, from Android!"}' $SLACK_URL
-  exit
-fi
+# if [[ -z ${APPCENTER_XCODE_PROJECT} && -z ${APPCENTER_XCODE_SCHEME} ]];
+# then
+#   echo "is Android"
+#   curl -X POST -H 'Content-type: application/json' --data '{"text":"Hello, from Android!"}' $SLACK_URL
+#   exit
+# fi
+
+COMMIT_ID=$(git rev-parse HEAD)
+
+echo $COMMIT_ID
+
+curl -X POST -H 'Content-type: application/json' --data '{"text":"Hello, from $COMMIT_ID!"}' $SLACK_URL
